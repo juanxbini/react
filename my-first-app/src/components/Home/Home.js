b3d7db95f9105f2fb984316479af9336490fd249
juanxbini <juanxbini@gmail.com>
2024-03-05 02:09:31 -0300

    Estilizacion de componentes con CSS

--- my-first-app/src/components/Home/Home.js
+++ my-first-app/src/components/Home/Home.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import './Home.css'
 
 class Home extends Component {
-    render(){
-        return <h1> I'm Home Component </h1>
+    render() {
+        return (
+            <div className="Home">
+                <h1> I'm Home Component </h1>
+                <p>Visit my site: <a href="github.com/juanxbini">GitHub</a></p>
+            </div>
+        );
     }
 }
 

f68dfa4b544cf4812e76db3ec578c56f160708b5
juanxbini <juanxbini@gmail.com>
2024-03-19 02:24:55 -0300

    metodo render, JSX y setDefaultTime

--- my-first-app/src/components/Timer/timer.js
+++ my-first-app/src/components/Timer/timer.js
@@ -9,14 +9,55 @@ class Timer extends Component {
         this.state = {
 
         }
+        this.times = {
+            defaultTime: 1500,
+            shortBreak: 300,
+            longBreak: 900
+        }
     }
 
     componentDidMount(){
-        
+        this.setDefaultTime();
     }
 
+    setDefaultTime = () =>{
+        this.setState({
+            time: this.times.defaultTime
+        });
+    }
+    
+
     render(){
-        return
+        return (
+            <div className='Pomodoro'>
+                <div className={`alert ${type}`}>
+                    {message}
+                </div>
+                <div className='timer'>
+                    {}
+                </div>
+                <div className='type'>
+                    <button
+                        className='start'
+                        onClick={''}
+                    >
+                        Start Working
+                    </button>
+                    <button
+                        className='short'
+                        onClick={''}
+                    >
+                        Short Break
+                    </button>
+                    <button
+                        className='long'
+                        onClick={''}
+                    >
+                        Long Break
+                    </button>
+                </div>
+            </div>
+        )
     }
 
 }

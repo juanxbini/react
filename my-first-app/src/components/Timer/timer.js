c27d7990d8ad3071144947f6efc4140e520049e2
juanxbini <juanxbini@gmail.com>
2024-03-19 02:38:17 -0300

    setTimeForWork setTimeForLongBreak setTimeForShortBreak

--- my-first-app/src/components/Timer/timer.js
+++ my-first-app/src/components/Timer/timer.js
@@ -7,8 +7,13 @@ class Timer extends Component {
         super();
 
         this.state = {
-
+            alert: {
+                type: '',
+                message: ''
+            },
+            time: 0
         }
+
         this.times = {
             defaultTime: 1500,
             shortBreak: 300,
@@ -17,15 +22,50 @@ class Timer extends Component {
     }
 
     componentDidMount(){
+        // Seteamos el tiempo por defecto
         this.setDefaultTime();
     }
 
     setDefaultTime = () =>{
+        // Actualizamos el estado con el tiempo por defecto.
         this.setState({
             time: this.times.defaultTime
         });
     }
     
+    setTimeForWork = () => {
+        // Actualizamos el estado con la alerta correspondiente
+        this.setState({
+            alert: {
+                type: 'start',
+                message: 'Start Working!'
+            }
+        })
+        // Seteamos el tipo de tiempo 
+        this.setTime(this.times.defaultTime)
+    }
+    setTimeForShortBreak = () => {
+        // Actualizamos el estado con la alerta correspondiente
+        this.setState({
+            alert: {
+                type: 'short',
+                message: 'Talking to the short break'
+            }
+        })
+        // Seteamos el tipo de tiempo 
+        this.setTime(this.times.shortBreak)
+    }
+    setTimeForLongBreak = () => {
+        // Actualizamos el estado con la alerta correspondiente
+        this.setState({
+            alert: {
+                type: 'long',
+                message: 'Talking to the long break'
+            }
+        })
+        // Seteamos el tipo de tiempo 
+        this.setTime(this.times.longBreak)
+    }
 
     render(){
         return (

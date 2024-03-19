afea3c45403ac3712725ea7d0404bebb64f9dac1
juanxbini <juanxbini@gmail.com>
2024-03-19 02:11:33 -0300

    Estructura inicial del componente Timer

---
+++ my-first-app/src/components/Timer/timer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Timer.css';
+
+class Timer extends Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount(){
+        
+    }
+
+    render(){
+        return
+    }
+
+}
+
+export default Timer;
